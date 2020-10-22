3dda7365de291d8987d8bb1e881940f58f4210b3
Christoph Weinsheimer <christoph.weinsheimer@esrlabs.com>
2020-10-22 16:50:10 +0200

    Add MatchedList PTP message matcher

---
+++ gptp/utils.py
@@ -0,0 +1,97 @@
+from .layers import PTPv2
+
+
+def matching_sync(sync, fup):
+    matching_portIds = (sync.sourcePortIdentity == fup.sourcePortIdentity)
+    matching_seqIds = (sync.sequenceId == fup.sequenceId)
+    return matching_portIds and matching_seqIds
+
+
+def matching_pdelay(req, resp):
+    matching_portIds = (req.sourcePortIdentity == resp.requestingPortIdentity)
+    matching_seqIds = (req.sequenceId == resp.sequenceId)
+    return matching_portIds and matching_seqIds
+
+
+class MatchedList:
+
+    def __init__(self):
+        self._sync = []
+        self._pdelay = []
+
+        self._unmatched_sync = []
+        self._unmatched_fup = []
+        self._unmatched_pdreq = []
+        self._unmatched_pdresp = []
+        self._unmatched_pdresp_fup = []
+
+    def add(self, pkt):
+        if type(pkt) == PTPv2:
+            self._add_dispatch(pkt)
+        elif iter(pkt):
+            for p in pkt:
+                self._add_dispatch(p)
+
+    def _add_dispatch(self, p):
+        if p.messageType == 0x0:
+            self._add_sync(p)
+        elif p.messageType == 0x2:
+            self._add_pdelay_request(p)
+        elif p.messageType == 0x3:
+            self._add_pdelay_response(p)
+        elif p.messageType == 0x8:
+            self._add_followup(p)
+        elif p.messageType == 0xA:
+            self._add_pdelay_response_followup(p)
+
+    def _add_sync(self, sync):
+        for i, fup in enumerate(self._unmatched_fup):
+            if matching_sync(sync, fup):
+                self._sync.append((sync, self._unmatched_fup.pop(i)))
+                return
+        self._unmatched_sync.append(sync)
+
+    def _add_followup(self, fup):
+        for i, sync in enumerate(self._unmatched_sync):
+            if matching_sync(sync, fup):
+                self._sync.append((self._unmatched_sync.pop(i), fup))
+                return
+        self._unmatched_fup.append(fup)
+
+    def _add_pdelay_request(self, req):
+        for i, resp in enumerate(self._unmatched_pdresp):
+            if matching_pdelay(req, resp):
+                for j, respfup in enumerate(self._unmatched_pdresp_fup):
+                    if matching_pdelay(req, respfup):
+                        self._pdelay.append(
+                            (req, self._unmatched_pdresp.pop(i), self._unmatched_pdresp_fup.pop(j)))
+                        return
+        self._unmatched_pdreq.append(req)
+
+    def _add_pdelay_response(self, resp):
+        for i, req in enumerate(self._unmatched_pdreq):
+            if matching_pdelay(req, resp):
+                for j, respfup in enumerate(self._unmatched_pdresp_fup):
+                    if matching_pdelay(req, respfup):
+                        self._pdelay.append(
+                            (self._unmatched_pdreq.pop(i), resp, self._unmatched_pdresp_fup.pop(j)))
+                        return
+        self._unmatched_pdresp.append(resp)
+
+    def _add_pdelay_response_followup(self, resp_fup):
+        for i, req in enumerate(self._unmatched_pdreq):
+            if matching_pdelay(req, resp_fup):
+                for j, resp in enumerate(self._unmatched_pdresp):
+                    if matching_pdelay(req, resp):
+                        self._pdelay.append(
+                            (self._unmatched_pdreq.pop(i), self._unmatched_pdresp.pop(j), resp_fup))
+                        return
+        self._unmatched_pdresp_fup.append(resp_fup)
+
+    @property
+    def sync(self):
+        return self._sync
+
+    @property
+    def pdelay(self):
+        return self._pdelay
---
+++ tests/test_utils.py
@@ -0,0 +1,69 @@
+import unittest
+
+from gptp.layers import PTPv2
+from gptp.fields import PortIdentityField
+from gptp.utils import MatchedList
+
+portId1 = PortIdentityField.from_mac("11:11:11:11:11:11", 1)
+portId2 = PortIdentityField.from_mac("22:22:22:22:22:22", 2)
+
+sync_p1_seq23 = PTPv2(messageType=0x0, sourcePortIdentity=portId1, sequenceId=23)
+fup_p1_seq23 = PTPv2(messageType=0x8, sourcePortIdentity=portId1, sequenceId=23)
+
+sync_p1_seq24 = PTPv2(messageType=0x0, sourcePortIdentity=portId1, sequenceId=24)
+fup_p1_seq24 = PTPv2(messageType=0x8, sourcePortIdentity=portId1, sequenceId=24)
+
+pdreq_p2_seq25 = PTPv2(messageType=0x2, sourcePortIdentity=portId2, sequenceId=25)
+pdresp_p1_seq25 = PTPv2(
+    messageType=0x3, sourcePortIdentity=portId1, sequenceId=25, requestingPortIdentity=portId2)
+pdrespfup_p1_seq25 = PTPv2(
+    messageType=0xA, sourcePortIdentity=portId1, sequenceId=25, requestingPortIdentity=portId2)
+
+pdreq_p2_seq26 = PTPv2(messageType=0x2, sourcePortIdentity=portId2, sequenceId=26)
+pdresp_p1_seq26 = PTPv2(
+    messageType=0x3, sourcePortIdentity=portId1, sequenceId=26, requestingPortIdentity=portId2)
+pdrespfup_p1_seq26 = PTPv2(
+    messageType=0xA, sourcePortIdentity=portId1, sequenceId=26, requestingPortIdentity=portId2)
+
+
+class MatchedListTest(unittest.TestCase):
+
+    def test_finds_matching_sync_and_followup_messages(self):
+        with self.subTest("adding single packets"):
+            matcher = MatchedList()
+            matcher.add(sync_p1_seq23)
+            self.assertEqual(0, len(matcher.sync))
+            matcher.add(fup_p1_seq23)
+            self.assertEqual(1, len(matcher.sync))
+
+        with self.subTest("adding as list"):
+            matcher = MatchedList()
+            matcher.add([sync_p1_seq24, fup_p1_seq24])
+            self.assertEqual(1, len(matcher.sync))
+
+        with self.subTest("wrong order"):
+            matcher = MatchedList()
+            matcher.add([fup_p1_seq24, sync_p1_seq24])
+            self.assertEqual(1, len(matcher.sync))
+
+    def test_finds_matching_pdelay_messages(self):
+        with self.subTest("adding single packets"):
+            matcher = MatchedList()
+            matcher.add(pdreq_p2_seq25)
+            self.assertEqual(0, len(matcher.pdelay))
+            matcher.add(pdresp_p1_seq25)
+            self.assertEqual(0, len(matcher.pdelay))
+            matcher.add(pdrespfup_p1_seq25)
+            self.assertEqual(1, len(matcher.pdelay))
+
+        with self.subTest("wrong order"):
+            matcher = MatchedList()
+            matcher.add([
+                pdreq_p2_seq26,
+                pdresp_p1_seq25,
+                pdrespfup_p1_seq25,
+                pdrespfup_p1_seq26,
+                pdresp_p1_seq26,
+                pdreq_p2_seq25
+            ])
+            self.assertEqual(2, len(matcher.pdelay))
